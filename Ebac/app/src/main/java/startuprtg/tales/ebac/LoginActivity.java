f70fa93de9726c49c228279bb8fe5eb9dd1f49fe
Talesk <talesd.guimaraes@gmail.com>
2018-08-20 01:59:03 -0300

    Change background Login

--- Ebac/app/src/main/java/startuprtg/tales/ebac/LoginActivity.java
+++ Ebac/app/src/main/java/startuprtg/tales/ebac/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -11,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 /**
@@ -29,10 +31,15 @@ public class LoginActivity extends AppCompatActivity {
     public static final String Login = "loginKey";
     public static final String Password = "pwdKey";
 
+    RelativeLayout activity_loginn;
+    AnimationDrawable animationDrawable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        backgroundAnimation();
 
         activity = this;
         sessionManager = new SessionManager(this);
@@ -96,6 +103,14 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(context, cls);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+    }
+
+    public void backgroundAnimation(){
+        activity_loginn = (RelativeLayout) findViewById(R.id.activity_main);
+        animationDrawable = (AnimationDrawable) activity_loginn.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
     }
 
     public boolean isOnline() {
--- Ebac/app/src/main/java/startuprtg/tales/ebac/MainActivity.java
+++ Ebac/app/src/main/java/startuprtg/tales/ebac/MainActivity.java
@@ -3,21 +3,30 @@ package startuprtg.tales.ebac;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.EventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout activity_loginn;
+    AnimationDrawable animationDrawable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
         if(isOnline()) {
             SessionManager sessionManager = new SessionManager(this);
@@ -30,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         finish();
     }
+
 
     public boolean isOnline() {
         ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
